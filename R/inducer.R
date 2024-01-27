4d94663863bd8dfa87868c7db6457b509022c957
noname-te <teresa.rupprecht@campus.lmu.de>
2024-01-27 19:58:56 +0100

    Added something.

--- R/inducer.R
+++ R/inducer.R
@@ -1,9 +1,39 @@
+# Environment
+ind <- new.env()
+
+xgboost <- function(x) {
+  x^2
+}
+
+# Assign the function to the environment
+ind$xgboost <- xgboost
+
+for (i in inducer_names) {
+  assign(i, get(i), envir = ind)
+}
 
 
-Inducer <- function() {
+
+Inducer <- function(name, configuration, ...) {
+  # TODO: assert
+  inducer <- function(){1+2}
+  class(inducer) <- c("Inducer", "function")
+  inducer
+}
 
 
-  structure(list(),
-            class = "Model")
+InducerXGBoost <- function(){
+  # TODO assert
+  inducerxgb <- Inducer() # TODO input Inducer
+  class(inducerxgb) <- c("InducerXGBoost", class(inducerxgb))
+  inducerxgb
+}
 
+print.Inducer <- function(inducer, ...) {
+  # TODO assert??
+  
+  cat("Inducer:", inducer$name, "\n")
+  cat("Configuration:", paste(names(inducer$configuration), "=", unlist(inducer$configuration),
+                              collapse = ", "))
+  invisible(inducer)
 }
