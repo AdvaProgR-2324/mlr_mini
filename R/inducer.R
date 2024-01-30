b4e8b454dea73a45d442982c6448055d759578b0
Felix Schweikl <f.schweikl@campus.lmu.de>
2024-01-30 17:31:23 +0100

    Inducer structure

--- R/inducer.R
+++ R/inducer.R
@@ -1,12 +1,12 @@
-# Environment
-ind <- new.env()
+# # Environment
+# ind <- new.env()
 
 xgboost <- function(x) {
   x^2
 }
 
 # Assign the function to the environment
-ind$xgboost <- xgboost
+# ind$xgboost <- xgboost
 
 for (i in inducer_names) {
   assign(i, get(i), envir = ind)
@@ -16,25 +16,46 @@ for (i in inducer_names) {
 
 Inducer <- function(name, configuration, ...) {
   # TODO: assert
-  inducer <- function(){1+2}
+  inducer <- function(name, configuration, ...) {
+
+    structure(
+      list(
+        name = name,
+        configuration = configuration
+      ), class = "Inducer"
+    )
+
+  }
+
+
   class(inducer) <- c("Inducer", "function")
-  inducer
+  inducer(name, configuration, ...)
+
 }
 
 
 
 print.Inducer <- function(inducer, ...) {
   # TODO assert??
-  
+
   cat("Inducer:", inducer$name, "\n")
   cat("Configuration:", paste(names(inducer$configuration), "=", unlist(inducer$configuration),
                               collapse = ", "))
   invisible(inducer)
 }
 
-InducerXGBoost <- function(){
+
+InducerXGBoost <- function() {
   # TODO assert
-  inducerxgb <- Inducer() # TODO input Inducer
+
+
+  inducerxgb <- Inducer(
+    name = "InducerXGBoost",
+    configuration = list(a = 2, b = 3),
+    params = list(c = 4, d = 5),  # TODO warum funktioniert das hier nicht???
+    testlol = "sdfgsfs"
+
+  ) # TODO input Inducer
   class(inducerxgb) <- c("InducerXGBoost", class(inducerxgb))
   inducerxgb
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,43 @@
+#' @import checkmate
+#' @import xgboost
+#' @import ranger
+#' @import rpart
+"_PACKAGE"
+
+# lm nicht, lol
+
+
+
+## import die anderen Packages
+
+
+
+.onLoad <- function(libname, pkgname) {
+
+  ind <- new.env(parent = emptyenv())
+  # attach(ind, pos = 2, name = "ind")
+
+  # ggf backports::import(pkgname)
+
+
+
+
+
+
+
+}
+
+
+# Inducer zuweisen ausserhalb ?
+ind$xgboost <- InducerXGBoost # Inducer zuweisen
+
+
+
+# Optional: onUnload-Funktion für Bereinigungen beim Entladen des Pakets
+.onUnload <- function(libpath) {
+  # Führe hier Bereinigungsaktionen durch, falls erforderlich
+
+  detach(pos = 2, name = "ind")
+}
 
 
