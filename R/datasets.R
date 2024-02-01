bb2fe45daacf8ddef45c3afa30158a6ff2fbdedb
SAkouete <S.Akouete@campus.lmu.de>
2024-02-01 12:25:01 +0100

    ##

--- R/datasets.R
+++ R/datasets.R
@@ -2,10 +2,11 @@
 
 Dataset <- function(data, target, type)
 {
-  Dataset$new(data, target)
+  Dataset$new(data, target, type)
   #Dataset is an object 
   # it has as arguments the data, target and type of task ( regression vs classification ) + name
   
   #split dataset into target and features 
   subset(data, select = target)
 }
+Dataset()
