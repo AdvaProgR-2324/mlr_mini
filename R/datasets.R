b4190ea2b394ec08d4ac17c460873bcbc9fd93d8
SAkouete <S.Akouete@campus.lmu.de>
2024-02-01 15:49:56 +0100

    Dataset gesplittet

--- R/datasets.R
+++ R/datasets.R
@@ -1,12 +1,22 @@
 ######### Write a function for the dataset and test that on the car data #########
 
-Dataset <- function(data, target, type)
+Dataset <- function(dat, target, type)
 {
-  Dataset$new(data, target, type)
-  #Dataset is an object 
-  # it has as arguments the data, target and type of task ( regression vs classification ) + name
   
-  #split dataset into target and features 
-  subset(data, select = target)
+  #Subset dataset so it can be splitted into features and target . It is useful to implement the [-function for this dataset for subsetting.
+  dat <-  subset(dat, select=sapply(dat, is.numeric))
+  print(dat)
+  target <- strsplit(as.character(dat), " ")
+  print(target)
+  
+  #Dataset is an object 
+  # it has as arguments the data, target and 
+  #type of task ( regression vs classification ) + name
+  
+ # switch(mode(type),  regression = "regr", classification = "class")
+  
 }
-Dataset()
+ca <- Dataset(cars, "dist")
+print(ca)
+?mtcars
+
