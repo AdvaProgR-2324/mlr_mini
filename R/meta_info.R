d5a23279f93d5d50f7ff0e55c26704c70de3dd8c
SAkouete <S.Akouete@campus.lmu.de>
2024-02-03 22:48:10 +0100

    Meta Info wird besser und besser :D

--- R/meta_info.R
+++ R/meta_info.R
@@ -1,21 +1,18 @@
 #' Print the meta info of a dataset
 #'
-#' This function creates a Dataset object based on a given matrix/data.frame with data.
-#'
-#' @param data A matrix or data.frame object with relevant data and named columns.
-#' @param target A string of a column name of data specifying the target.
-#' @param type A string specifying whether a regression or classification should be done.
-#'
-#' @return A object of class 'Dataset'.
+#' TO-DO
+#' @return prints out the neccessary information
 #'
 #' @export
-metainfo.Dataset <- function(data, target, type = NULL,  name = as.name(deparse(substitute(data), 20)[[1]])) {
- x <-  structure(list(name = name, data = data, target = target, type = type, nrow = nrow(data), missings = is.na(data)), class = "Dataset")
- print(x) 
+metainfo.Dataset <- function(data, target = data$target,  type = data$type,  name = as.name(deparse(substitute(data), 20)[[1]])) {
+  # if (target %in% names(data$data)) {
+  #  data$feature <-  if (is.factor(data[[target]])) "target" else "feature"
+  # }
+ 
+ x <-  structure(list(name = as.character(name), target = data$target, type = data$type, nrow = nrow(data$data), missings = anyNA(data)), class = "Dataset")
+ print(x)
  
 }
-print(cars.data)
 
-metainfo.Dataset(cars.data, target = "dist")
-colnames(cars.data)
-typeof(cars.data)
+metainfo.Dataset(cars.data)
+
