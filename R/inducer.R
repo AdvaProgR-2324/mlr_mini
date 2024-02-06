7e627e1da7c2057b3689f3b56a18f75a00e47409
noname-te <teresa.rupprecht@campus.lmu.de>
2024-02-06 16:57:09 +0100

    Updated configuration function and changed some descriptions.

--- R/inducer.R
+++ R/inducer.R
@@ -2,7 +2,8 @@
 #' @description Build an Inducer
 #' @param name The name of the inducer
 #' @param configuration The configuration of the inducer.
-#' @hyperparameter A named list containing the hyperparameters of the inducer.
+#' @param defaults The default values of the inducer.
+#' @param hyperparameter A named list containing the hyperparameters of the inducer.
 #' The list should contain the names of the hyperparameters, the type of the hyperparameters,
 #' the lower and upper bound of the hyperparameter range as well as a default value.
 #* @export
@@ -33,12 +34,6 @@ print.Inducer <- function(inducer, ...) {
   # TODO: print Configuration only if it was changed.
 
   cat("Inducer:", inducer$name, "\n")
-  # cat("Configuration:", paste(names(inducer$configuration), "=", unlist(inducer$configuration), collapse = ", "))
-
-  # NEU mit configuration function
-  # cat("Configuration:", paste(names(configuration(inducer)), "=", unlist(configuration(inducer)), collapse = ", "))
-
-  # nochmal NEU
   cat("Configuration:", paste(names(configuration(inducer)), "=", as.vector(configuration(inducer)), collapse = ", "))
 
   invisible(inducer)
@@ -46,11 +41,10 @@ print.Inducer <- function(inducer, ...) {
 
 
 
-
-
 #' @title Configuration print function for an Inducer object
 #' @description Print the configuration of an Inducer.
 #' @param inducer An inducer being an Inducer object.
+#' @value The current configuration of the Inducer.
 #' @export
 configuration <- function(inducer) {
   return(inducer$configuration)
@@ -64,10 +58,19 @@ configuration <- function(inducer) {
 `configuration<-` <- function(inducer, value) {
   names_inducer_config <- names(inducer$configuration)
   names_value <- names(value)
+  print("VALUES:")
+  print(value)
+  print("INDUCER")
+  print(inducer)
   if (all(names_value %in% names_inducer_config)) {
     for (name in names_value) {
-      # TODO checken if type of value is correct and in the range of hyperparameter
-      inducer$configuration[[name]] <- value[[name]]
+      print(value[[name]])
+      if (is.null(value[[name]]) || is.name(value[[name]]) || class(value[[name]]) == inducer$hyperparameter[[name]]$type) {
+        # TODO: check if value is in range
+        inducer$configuration[[name]] <- value[[name]]
+      } else if (class(value[[name]]) != inducer$hyperparameter[[name]]$type) {
+        stop(paste("Error in `configuration<-`: invalid class for variable", name))
+      }
     }
     return(inducer)
   } else {
