8ddd9ea92bca22f83f39df000ec274ce144d7dce
Felix Schweikl <f.schweikl@campus.lmu.de>
2024-02-10 16:52:20 +0100

    fixed typo

--- tests/testthat/testDataset.R
+++ tests/testthat/testDataset.R
@@ -1,8 +1,8 @@
 test_that(desc = "Correct dataset", code = {
-  cars_ds <- Dataset(cars, target = "dist")
+  cars_ds <- Dataset(datasets::cars, target = "dist")
 
   expect_equal(cars_ds$target, "dist")
-  expect_equal(cars_ds$data, cars)
+  expect_equal(cars_ds$data, datasets::cars)
 
 
 })
