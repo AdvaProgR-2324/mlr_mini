bbf902ce2a5b8f2e51f3ff17b013928ea8fd3e6e
Felix Schweikl <f.schweikl@campus.lmu.de>
2024-02-11 20:39:35 +0100

    predict half

--- R/Models.R
+++ R/Models.R
@@ -7,13 +7,13 @@
 #' @export
 
 Model <- function(inducer.name, inducer.configuration, data.name, data.target, data.features,
-                   fitted.values, coefficients) {
+                   fitted.values = NULL, coefficients = NULL, model.out, model.data) {
   #TODO: further assertions for data.target -> rather in ModelRegression or ModelClassification
   # data.features and fitted.values
   assert_string(inducer.name)
   assert_list(inducer.configuration)
   assert_character(data.name)
-  assert_numeric(coefficients)
+  # assert_numeric(coefficients)
   structure(list(
     inducer.name = inducer.name,
     inducer.configuration = inducer.configuration,
@@ -22,7 +22,8 @@ Model <- function(inducer.name, inducer.configuration, data.name, data.target, d
     data.features = data.features,
     fitted.values = fitted.values,
     coefficients = coefficients,
-    model.out = model.out
+    model.out = model.out,
+    mode.data = model.data
   ), class = "Model"
   )
 }
@@ -40,7 +41,7 @@ configuration <- function(...) {
 #' @export
 configuration.Model <- function(model, ...) {
   assert_class(model, "Model") # TODO: is the assert_class necessary if we use a generic?? I dont think so
-  return(model$configuration)
+  return(model$inducer.configuration)
 }
 
 
@@ -112,6 +113,56 @@ predict.Model <- function(model, newdata, ...) {
   return(ind)
 }
 
+predict.ModelXGBoost <- function(model, newdata, ...) {
+
+
+  fittedModel <- model$model.out
+  dataModel <- modelObj$mode.data$data
+
+  ## newdata into datamatrix
+  if (class(newdata) ==  "data.frame") {  # if dataframe: only vector with prediction values
+    # TODO asserts, dataframe must have same features as dataset in fittedmodel
+    data_n_df <- as.matrix(newdata)
+    fittedVals <- xgboost:::predict.xgb.Booster(object = fittedModel, newdata = data_n_df)
+
+  } else if (class(newdata) == "Dataset") {  # if Dataset: new dataframe with prediction (values from predict function) and truth (dataset)
+    data_n_ds <- as.matrix(newdata$data)
+
+    fitted_ds <- xgboost:::predict.xgb.Booster(object = fittedModel, newdata = data_n_df)
+
+
+
+  } else {
+    stop("Type of dataset not supported")  # class(newdata)
+  }
+
+
+  # return in if
+  return(NULL)
+
+
+}
+
+
+cars_ds <- Dataset(cars, target = "dist")
+model <- InducerXGBoost(.data = cars_ds)
+class(model)
+newdata <- data.frame(speed = 10)
+newdata <- cars_ds$data[c(1, 2), ]
+data_n_ds <- cars_ds[c(1, 2, 3, 4), ]  # hier newdata
+data_n_ds$data[, data_n_ds$target]
+
+
+xgboost:::predict.xgb.Booster(object = fittedModel, newdata = as.matrix(data.frame(speed = 10)))
+
+
+
+
+
+
+
+
+
 
 #' @title Fit function
 fit <- function(...) {
@@ -164,13 +215,22 @@ fit.InducerXGBoost <- function(.inducer, .data, ...) {
   # data <- as.matrix(.data)
   # fittedModel <- xgboost(data = data, label = data[, "dist"], nrounds = xgb_nRound, params = pastedHyperparam)
 
-  fittedModel <- xgboost(data = as.matrix(.data$data), label = .data$data[, .data$target], nrounds = xgb_nRound,
+  featureVars <- setdiff(colnames(.data$data), .data$target)
+  fittedModel <- xgboost(data = as.matrix(.data$data[, featureVars]), label = .data$data[, .data$target], nrounds = xgb_nRound,
                          params = pastedHyperparam)
 
+  modelObj <- Model(inducer.name = "InducerXGBoost",
+                    inducer.configuration = configuration(.inducer),
+                    data.name = as.character(.data$name),
+                    data.target = .data$target,
+                    data.features = colnames(.data$data),  # change feature names automatic
+                    model.out = fittedModel,
+                    model.data = .data
 
+                    )
+  class(modelObj) <- c("ModelXGBoost", "ModelRegression", "Model")
 
-
-  return(fittedModel)
+  return(modelObj)
   # fit.InducerXGBoost(InducerXGBoost(.data = cars))
   # fit.InducerXGBoost(InducerXGBoost(), .data = cars, nrounds = 3)  # funktioniert
   ## FUNKTIONIERT
@@ -199,17 +259,22 @@ fit.InducerLm <- function(.inducer, .data, ...) {
   return(fittedModel)  # return fitted model
 }
 
-.inducer <- InducerLm()
+# .inducer <- InducerLm()
+#
+# .data <- Dataset(cars, target = "dist")
+# .data$data[, .data$target]
 
-.data <- Dataset(cars, target = "dist")
-.data$data[, .data$target]
-
-# TODO: Isn't that the same as in line 62 ff?
+# TODO: Isn't that the same as in line 62 ff? -> no?
 modelObject <- function(model) {
   assert_class(model, "Model")
   # TODO asserts
 
-  print(modelObj)
+  print(modelObj$model.out)  # works !
 
 
+ # modelObject(model)
 }
+
+
+
+
