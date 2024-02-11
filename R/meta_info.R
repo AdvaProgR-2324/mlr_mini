05a2c761ee0649bb2040e9c3b3b6038d2b26a890
SAkouete <S.Akouete@campus.lmu.de>
2024-02-11 20:04:02 +0100

    still fixing some issues

--- R/meta_info.R
+++ R/meta_info.R
@@ -4,33 +4,22 @@
 #' @return prints out the neccessary information
 #'
 #' @export
-metainfo.Dataset <- function(data, target = data$target,  type = data$type,  name = as.name(deparse(substitute(data), 20)[[1]]), ...) {
+metainfo.Dataset <- function(data, targets = data$target,  type = data$type,  name = as.name(deparse(substitute(data), 20)[[1]]), ...) {
 
-  # if (target %in% names(data$data)) {
-  #  data$feature <-  if (is.factor(data[[target]])) "target" else "feature"
-  # }
-  #Split dataset info target and feature 
+ 
+#assert(feature %in% names(data$data))
+  
+ data$features <- setdiff(names(data$data), "target")  # Exclude the target column
+ # Verify that column1 and column2 are not the same
+# assert_equal(data$target, data$feature, msg = "Columns target and feature should not be the same.")
   
   #data$feature <- ifelse(target %in% names(data$data), "target", "feature")
  
- x <-  structure(list(name = as.character(name), target = data$target, type = data$type, nrow = nrow(data$data), missings = anyNA(data)), class = "Dataset")
+ x <-  structure(list(name = as.character(name), target = data$target, features = data$features, type = data$type, nrow = nrow(data$data), missings = anyNA(data)), class = "Dataset")
  print(x)
  
 }
-#To Do: Datensatz in target und feature aufteilen 
-
 cars.data$data
-cars.data$target
 cars.data$feature %in% names(cars.data$feature)
 metainfo.Dataset(cars.data)
-if (!cars.data$target %in% names(cars.data$data)) {
-  stop(sprintf("Target column %s could not be found in data", target))
-}
-cars.data$data
-print(cars.data)
-cars.data[cols] <- do.call(rbind, strsplit(as.character(cars.data$data), '')) 
 
-unlist(strsplit(names(cars.data$data), " "))
-cars.data$target
-cars.data$feature
-         
