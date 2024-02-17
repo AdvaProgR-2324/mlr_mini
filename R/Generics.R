a8fe27220a3c0c3938be961f533249174d9b47c8
noname-te <teresa.rupprecht@campus.lmu.de>
2024-02-17 18:27:07 +0100

    Added generics.

---
+++ R/Generics.R
@@ -0,0 +1,23 @@
+#' @title S3 method fit
+#' @export
+configuration <- function(...) {
+  UseMethod("configuration")
+}
+
+#' @title S3 method fit
+#' @export
+hyperparameters <- function(...) {
+  UseMethod("hyperparameters")
+}
+
+#' @title S3 method fit
+#' @export
+fit <- function(...) {
+  UseMethod("fit")
+}
+
+#' @title S3 method predict
+#' @export
+predict <- function(...) {
+  UseMethod("predict")
+}
