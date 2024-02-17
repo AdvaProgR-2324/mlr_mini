16be8b604ff54638b0981143ad996c22605cfdac
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2024-02-17 14:04:25 +0100

    added print and changed splitcv

--- R/resampling.R
+++ R/resampling.R
@@ -1,26 +1,42 @@
 SplitCV <- function(folds, repeats = 1) {
   
   splitcv <- function(.data) {
-    n <- nrow(.data)
+    checkmate::assertClass(.data, "Dataset")
+    
+    n <- nrow(.data$data)
     n.train <- ceiling(n / folds)
     result <- list()
     for (repetition in seq(repeats)) {
       indices <- sample(seq(n))
       result[[repetition]] <- list(training = indices[1:n.train], validation = indices[-(1:n.train)])
     }
-    structure(result,class = c("SplitInstanceCV", "SplitInstance"))
+    class(result) <- c("SplitInstanceCV", "SplitInstance")
+    result_env <- list2env(as.list(environment(splitcv), all.names = TRUE), parent = emptyenv())
+    assign("name", .data$name, envir = result_env)
+    assign("dim", dim(.data$data), envir = result_env)
+    environment(result) <- result_env
+    result
   }
-  
   hyperparameters = list(folds = folds, repeats = repeats) 
   env <- list2env(list(hyperparameters = hyperparameters,
-                       cv = splitcv,
-                       parent = emptyenv()))
+                       cv = splitcv))
   class(splitcv) <- c("SplitCV", "Split")
   environment(splitcv) <- env
   splitcv
-  
 }
+
 Split <- function() {
-  structure(list(cv = SplitCV),
-            class = "Split")
+  env <- list2env(list(
+    cv = SplitCV
+  ))
+  structure(env, class = "Split")
 }
+
+print.SplitInstanceCV <- function(x, ...) {
+  hyperparameters <- get("hyperparameters", envir = environment(x))
+  name <- get("name", envir = environment(x))
+  dim <- get("dim", envir = environment(x))
+  cat(paste0('CV Split Instance of the "', name, '" dataset', '(', dim[1], ") rows"))
+  invisible(x)
+}
+
