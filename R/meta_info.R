c823a71f405450acc2afbcaae6f21246cf607321
SAkouete <S.Akouete@campus.lmu.de>
2024-02-18 16:07:56 +0100

    nix neues

--- R/meta_info.R
+++ R/meta_info.R
@@ -1,4 +1,9 @@
-#' Print the meta info of a dataset
+#' Print the meta Information of a dataset
+#' 
+#' @param data A dataset
+#' @param target The target variable of a dataset
+#' @param feature Feature variable of a dataset
+#' @param type Regression or Classification
 #'
 #' TO-DO
 #' @return prints out the neccessary information
@@ -6,7 +11,7 @@
 #' @export
 metainfo.Dataset <- function(data, targets = data$target,  type = data$type,  name = as.name(deparse(substitute(data), 20)[[1]]), features = data$features, ...) {
 
- data$features <-  setdiff(targets, features) # Exclude the target column
+# data$features <- setdiff(data$target, data$feature) # Exclude the target column
  
  x <-  structure(list(name = as.character(data$name), targets = data$target, class(targets),  features = data$features, type = data$type, nrow = nrow(data$data), missings = anyNA(data)), class = "Dataset")
  lapply(x, class)
@@ -15,4 +20,6 @@ metainfo.Dataset <- function(data, targets = data$target,  type = data$type,  na
 }
 
 metainfo.Dataset(cars.data)
+cars.data
+
 
