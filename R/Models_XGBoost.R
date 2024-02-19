5ddd44db144442bd668ded53739de13e20a8de74
mms1410 <92041267+mms1410@users.noreply.github.com>
2024-02-19 19:57:51 +0100

    Clear (#29)
    * styled
    
    * added lintr ignore in LOC 26

--- R/Models_XGBoost.R
+++ R/Models_XGBoost.R
@@ -33,7 +33,7 @@ fit.InducerXGBoost <- function(.inducer, .data = NULL, nrounds = 1, eta = 0.3, g
   stopifnot(".data muste be of class Dataset or data.frame" = class(.data)[2] %in% c("Dataset", "data.frame"))
 
   data_df <- as.data.frame(.data$data)
-  model <- xgboost
+  model <- xgboost # nolint
   original_call <- match.call(expand.dots = FALSE)
   form_Ind <- formals(.inducer) # formals of ind
   form_Ind$.data <- NULL # remove .data arg
@@ -96,7 +96,7 @@ predict.ModelXGBoost <- function(model, newdata, ...) {
   if (length(class(newdata)) > 1) {
     stopifnot(".data muste be of class Dataset or data.frame" = c("Dataset") %in% class(newdata))
   } else {
-    stopifnot(".data muste be of class Dataset or data.frame" = c("data.frame") ==  class(newdata))
+    stopifnot(".data muste be of class Dataset or data.frame" = c("data.frame") == class(newdata))
   }
 
 
