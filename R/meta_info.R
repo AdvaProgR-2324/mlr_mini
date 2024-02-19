838f690006978f8d89c920896e13a8d2e5f62cb8
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2024-02-19 16:24:27 +0100

    linted

--- R/meta_info.R
+++ R/meta_info.R
@@ -12,15 +12,18 @@
 metainfo.Dataset <- function(data, targets = data$target, type = data$type,
                              name = as.name(deparse(substitute(data), 20)[[1]]),
                              features = data$features, ...) {
-  
-  x <- structure(list(name = as.character(data$name),
-                      targets = data$target,
-                      class(targets),
-                      features = data$features,
-                      type = data$type,
-                      nrow = nrow(data$data),
-                      missings = anyNA(data)),
-                 class = "Dataset")
+  x <- structure(
+    list(
+      name = as.character(data$name),
+      targets = data$target,
+      class(targets),
+      features = data$features,
+      type = data$type,
+      nrow = nrow(data$data),
+      missings = anyNA(data)
+    ),
+    class = "Dataset"
+  )
   lapply(x, class)
   print(x)
 }
