94b1f4f0b43852e087aa3a490a80b67d6d1430ca
xFoxFelix <64030621+xFoxFelix@users.noreply.github.com>
2024-02-20 00:09:15 +0100

    lint style

--- R/inducer_InducerLm.R
+++ R/inducer_InducerLm.R
@@ -1,8 +1,10 @@
 #' @title Function to create an object of class `InducerLm`
-#' @description If .data is empty an `InducerLm` object will be created. If .data is a `Dataset` object a LM model will be fitted
+#' @description If .data is empty an `InducerLm` object will be created. If .data is a `Dataset` object
+#' a LM model will be fitted
 #' @seealso [fit.InducerLm()]
 #' @param .data data object of class `Dataset`.
-#' @param subset An optional argument. A vector specifying a subset of observations that should be used for fitting the model.
+#' @param subset An optional argument. A vector specifying a subset of observations that should be used
+#' for fitting the model.
 #' @param weights An optional argument. A vector of weights that should be used for fitting the model.
 #' @param na.action An optional argument. A function that specifies how to handle missing values.
 #' @param method The method which should be used for fitting. For more information see [lm]
@@ -11,7 +13,8 @@
 #' @param y if true y is returned
 #' @param qr if true the QR decomposition is returned
 #' @param singular.ok logical. If FALSE (the default in S but not in R) a singular fit is an error
-#' @param offset this can be used to specify an a priori known component to be included in the linear predictor during fitting.
+#' @param offset this can be used to specify an a priori known component to be included in the linear
+#' predictor during fitting.
 #' @return a `InducerLm` object
 #' @export
 #' @examples
@@ -25,7 +28,7 @@
 #' fittedInd <- InducerLm(.data = cars.data)
 #' fittedInd
 InducerLm <- function(.data = NULL, formula, subset, weights, na.action, method = "qr", model = TRUE,
-                      x = FALSE, y = FALSE, qr = TRUE, singular.ok = TRUE, offset) {  # contrasts = NULL,
+                      x = FALSE, y = FALSE, qr = TRUE, singular.ok = TRUE, offset) { # ggf. contrasts = NULL,
   # assert
   if (!is.null(.data)) {
     assert_class(x = .data, classes = "Dataset")
@@ -71,22 +74,7 @@ print.InducerLm <- function(x, ...) {
 
 
 
-
-## TODO wennfit.InducerLm fertig dann in Models_modelLm.R verschieben
-#  fit.InducerLm(.inducer = InducerLm(), .data = cars_ds)
-
-# model <- fit.InducerLm(.inducer = InducerLm(), .data = cars_ds)
-# newdata <- cars_ds[c(1, 2, 3, 4), ]
-# newdata <- data.frame(speed = 10)
-
-
-
-
-
-
-
-
-HyperparameterLm = list(
+HyperparameterLm <- list(
   data = list(name = "data", arg.null = TRUE),
   formula = list(name = "formula", type = "formula", arg.null = TRUE),
   subset = list(name = "subset", type = "logical", arg.null = FALSE), # TODO: type checken!!
@@ -99,4 +87,5 @@ HyperparameterLm = list(
   qr = list(name = "qr", type = "logical", arg.null = FALSE),
   singular.ok = list(name = "singular.ok", type = "logical", arg.null = FALSE),
   contrasts = list(name = "contrasts", type = "list", arg.null = FALSE),
-  offset = list(name = "offset", type = "numeric", arg.null = FALSE))
+  offset = list(name = "offset", type = "numeric", arg.null = FALSE)
+)
