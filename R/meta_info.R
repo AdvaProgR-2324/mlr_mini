1319b5ccb0d4983b9a152d719c83559925fa92c5
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2024-02-19 16:19:41 +0100

    reformatted

--- R/meta_info.R
+++ R/meta_info.R
@@ -9,10 +9,19 @@
 #' @return prints out the neccessary information
 #'
 #' @export
-metainfo.Dataset <- function(data, targets = data$target, type = data$type, name = as.name(deparse(substitute(data), 20)[[1]]), features = data$features, ...) {
+metainfo.Dataset <- function(data, targets = data$target, type = data$type,
+                             name = as.name(deparse(substitute(data), 20)[[1]]),
+                             features = data$features, ...) {
   # data$features <- setdiff(data$target, data$feature) # Exclude the target column
 
-  x <- structure(list(name = as.character(data$name), targets = data$target, class(targets), features = data$features, type = data$type, nrow = nrow(data$data), missings = anyNA(data)), class = "Dataset")
+  x <- structure(list(name = as.character(data$name),
+                      targets = data$target,
+                      class(targets),
+                      features = data$features,
+                      type = data$type,
+                      nrow = nrow(data$data),
+                      missings = anyNA(data)),
+                 class = "Dataset")
   lapply(x, class)
   print(x)
 }
