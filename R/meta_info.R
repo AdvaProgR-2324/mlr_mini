1eb646c933364b11b02964a825fd3da5b1216bda
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2024-02-19 18:06:40 +0100

    updated doc

--- R/meta_info.R
+++ R/meta_info.R
@@ -1,8 +1,7 @@
 #' Print the meta Information of a dataset
 #'
 #' @param data A dataset
-#' @param target The target variable of a dataset
-#' @param feature Feature variable of a dataset
+#' @param targets The target variable of a dataset
 #' @param type Regression or Classification
 #' @param name name
 #' @param features features
