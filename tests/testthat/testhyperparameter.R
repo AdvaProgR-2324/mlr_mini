80df3b4616241af4dfd9f57b7698b2fb1ee3d349
SAkouete <S.Akouete@campus.lmu.de>
2024-02-20 21:35:06 +0100

    ###

--- tests/testthat/testhyperparameter.R
+++ tests/testthat/testhyperparameter.R
@@ -1,7 +1,19 @@
 ######### Writing some tests ##### 
-testthat::equals(hyperparameter, labels = c("name", "type", "range") )
-hyperparameter(
-  learning_rate = c(0.001, 0.01, 0.1),
-  batch_size = c(1, Inf),
-  optimizer = c("adam", "sgd", "rmsprop"), z = factor(letters)
-)
+
+p_num <- function(a, b){
+  checkmate::assert_numeric(a, min = 1, all.missing = FALSE, finite = FALSE)
+  checkmate::assert_numeric(b, min = 1, all.missing = FALSE, finite = FALSE)
+}
+
+p_int <- function(a, b) {
+  checkmate::assert_integer(a)
+  checkmate::assert_integerish(b)
+}
+
+p_fct <- function(a) {
+  checkmate::assert_character(a)
+}
+
+# Call hp function to get information about p_num function arguments
+hpx <- hp(x = p_num(0, 1), y = p_int(1, Inf), z = p_fct(letters))
+print.hp(x = p_num(0,1))
