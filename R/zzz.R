a40494d074e4026afa64724b38e6f5719d8988df
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2024-02-20 20:32:15 +0100

    updated rpart

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
 #' @importFrom datasets cars
 #' @importFrom stats lm
 #' @importFrom xgboost xgboost
-#' @importFrom rpart rpart
+#' @importFrom rpart rpart na.rpart
 #' @importFrom pROC auc
 #' @importFrom pROC roc
 "_PACKAGE"
