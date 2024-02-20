96c5b307f6f11efc024e8436280271101c2d5a9f
xFoxFelix <64030621+xFoxFelix@users.noreply.github.com>
2024-02-20 18:48:18 +0100

    added some tests

--- tests/testthat/test_inducer.R
+++ tests/testthat/test_inducer.R
@@ -14,3 +14,20 @@ test_that("Test for class InducerXGBoost", {
   indxgb <- InducerXGBoost()
   testthat::expect_s3_class(indxgb, "InducerXGBoost")
 })
+
+test_that("Test for Inducer fit function", {
+  cars.data <- Dataset(data = cars, target = "dist")
+  fittedmod <- fit(.inducer = InducerXGBoost(), .data = cars.data)
+  fittedxgbmode <- xgboost::xgboost(data = as.matrix(cars), nrounds = 1, label = cars$dist)
+  expect_equal(class(modelObject(fittedmod)), class(fittedxgbmode))
+})
+
+test_that("Test for Inducer lm", {
+  lmmod <- lm(dist ~ speed, cars)
+  cars.data <- Dataset(data = cars, target = "dist")
+  lmmlrmini <- InducerLm(.data = cars.data)
+  
+  expect_equal(class(modelObject(lmmlrmini)), class(lmmod))
+  expect_equal(coefficients(lmmod), coefficients(modelObject(lmmlrmini)))
+  expect_equal(lmmlrmini$mode.data, cars.data)  # data equal
+})
