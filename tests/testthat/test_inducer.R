1d684e8e9117f6572c4e14f88bb3dbeb8d7c7f2f
xFoxFelix <64030621+xFoxFelix@users.noreply.github.com>
2024-02-20 16:57:16 +0100

    rm test

--- tests/testthat/test_inducer.R
+++ tests/testthat/test_inducer.R
@@ -14,10 +14,3 @@ test_that("Test for class InducerXGBoost", {
   indxgb <- InducerXGBoost()
   testthat::expect_s3_class(indxgb, "InducerXGBoost")
 })
-
-test_that("Test for Inducer Function", {
-  cars.data <- Dataset(data = cars, target = "dist")
-  fittedmod <- fit.InducerXGBoost(.inducer = InducerXGBoost(), .data = cars.data)
-  fittedxgbmode <- xgboost::xgboost(data = as.matrix(cars), nrounds = 1, label = cars$dist)
-  expect_equal(class(modelObject(fittedmod)), class(fittedxgbmode))
-})
