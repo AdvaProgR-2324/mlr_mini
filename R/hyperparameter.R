24d3afb8b41bab386656fb13fec492a528e27e7b
SAkouete <S.Akouete@campus.lmu.de>
2024-02-20 23:04:39 +0100

    ### fixed issues ###

--- R/hyperparameter.R
+++ R/hyperparameter.R
@@ -6,27 +6,31 @@
 hp <- function(...) {
   rm(list = ls())
   args <- list(...)
-  names <- names(args)
-  types <- unname(sapply(args, function(elem) elem$type))
-  ranges <- sapply(args, range)
-  #    {
-  #   if (class(x) == "numeric" || class(x) == "integer") {
-  #     paste("[", paste(range(x), collapse = ", "), "]", collapse = "")
-  #   } else if (class(x) == "character") {
-  #     paste("[", paste(unique(x), collapse = ", "), "]", collapse = "")
-  #   } else if (class(x) == "factor") {
-  #     paste("{", paste(sprintf("\"%s\"", levels(x)), collapse = ", "), "}", collapse = "")
-  #   } else {
-  #     "Not applicable"
-  #   }
-  # })
-  info <- data.frame(
-    name = names,
-    type = types,
-    range = ranges
+  arg_names <- names(args)
+  arg_types <- sapply(args, class)
+  arg_ranges <- sapply(args, function(x) {
+    if (class(x) %in% c("numeric", "integer")) {
+      paste("[", min(x), ", ", max(x), "]", sep = "")
+    } else if (is.character(x)) {
+      paste("{", paste(unique(x), collapse = ", "), "}", sep = "")
+    } else if (is.factor(x)) {
+      paste("{", paste(sprintf("\"%s\"", levels(x)), collapse = ", "), "}", sep = "")
+    } else {
+      "Not applicable"
+    }
+  })
+  info <- data.table::data.table(
+    name = arg_names,
+    type = arg_types,
+    range = arg_ranges,
+    stringsAsFactors = FALSE
   )
   class(info) <- "hp"
   return(info)
+  
+  # Convert data.table to data frame
+  info <- as.data.frame(info)
+  
 }
 
 
@@ -62,13 +66,13 @@ p_fct <- function(a) {
 
 print.hp <- function(x, ...) {
   # y <- list(...)
-  # input_names <- names(x)
-  #
+ # input_names <- names(x)
+  # 
   # # Check if the number of elements in y matches the length of x
   # if (length(y) != length(input_names)) {
   #   stop("Number of elements in 'y' does not match the length of 'x'")
   # }
-  #
+  # 
   # # Extract type values from y if available, otherwise set to NA
   # type_list <- sapply(y, function(elem) {
   #   if ("type" %in% names(elem)) {
@@ -77,20 +81,20 @@ print.hp <- function(x, ...) {
   #     NA
   #   }
   # })
-  #
+  # 
   # # Ensure type_list has the same length as input_names
   # if (length(type_list) < length(input_names)) {
-  #   type_list <- c(type_list, rep(NA, length(input_names) - length(type_list)))
-  # }
-  #
-  # output <- data.frame(name = input_names, type = as.character(type_list), range = as.character(range(x)))
-  # return(output)
-  return(x)
-  invisible(x)
+  #  type_list <- c(type_list, rep(NA, length(input_names) - length(type_list)))
+  # # }
+  # # 
+  #  output <- data.frame(name = input_names, type = as.character(type_list), range = range(x))
+  # return(x)
+ # invisible(output)
 }
 
 
 
 # Call hp function to get information about p_num function arguments
 hpx <- hp(x = p_num(0, 1), y = p_int(1, Inf), z = p_fct(letters))
-print(x = p_num(0, 1))
+print.hp(x = p_num(0, 1))
+
