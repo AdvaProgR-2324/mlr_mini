7e239fb95c1e98cf9ee7fb3d402bd7f5429924ce
mms1410 <92041267+mms1410@users.noreply.github.com>
2024-02-20 22:04:23 +0100

    Clear (#39)
    * styled
    
    * added lintr ignore in LOC 26
    
    * changed T to TRUE in LOC 82
    
    * updated
    
    * changed import
    
    * removed man

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 #' @import checkmate
 #' @import testthat
 #' @importFrom xgboost xgboost
-#' @importFrom rpart rpart na.part
+#' @importFrom rpart rpart 
+#' @importFrom rpart na.part
 #' @importFrom pROC auc roc
 "_PACKAGE"
